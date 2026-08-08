90177068e3d7832465a1ce49170599aa7129367f
Rudra Pratap Singh <singhrudra8957@gmail.com>
2026-08-09 03:02:49 +0530

    feat: scope task queries to authenticated users

--- backend/src/main/kotlin/com/taskflow/task/TaskRepository.kt
+++ backend/src/main/kotlin/com/taskflow/task/TaskRepository.kt
@@ -2,4 +2,8 @@ package com.taskflow.task
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface TaskRepository : JpaRepository<Task, Long>
+interface TaskRepository : JpaRepository<Task, Long> {
+    fun findAllByUserEmail(email: String): List<Task>
+    fun findByIdAndUserEmail(id: Long, email: String): Task?
+    fun existsByIdAndUserEmail(id: Long, email: String): Boolean
+}
